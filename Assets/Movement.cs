05255eb438aa32c49ca9fe69cb6c6105013fa530
knasts <kulyk.anastasiia1@lll.kpi.ua>
2026-01-30 23:15:01 +0200

    now it jumps!!

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public enum Speeds { Slow, Normal, Fast, Faster, Fastest};
 public class Movement : MonoBehaviour
 {
-    public Speeds CurrentSpeed; 
+    public Speeds CurrentSpeed;
+    public Transform GroundCheckTransform;
+    public float GroundCheckRadius;
+    public LayerMask GroundMask;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     float[] SpeedValues = { 8.6f, 10.4f, 12.96f, 15.6f, 19.27f };
 
     private void Update()
     {                                               // 1/10 FPS = 0.1 time.deltatime, so won't move too fast           
         transform.position += Vector3.right * SpeedValues[(int)CurrentSpeed] * Time.deltaTime;
+        float jumpForce = 26.6581f;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(OnGround())
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+    }
+    bool OnGround()
+    {
+        return Physics2D.OverlapCircle(GroundCheckTransform.position, GroundCheckRadius, GroundMask);
     }
 }
